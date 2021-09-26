72d43ac120db7ed6bcc66155670ec2da36ba2b29
Chappellm18 <Chapps360@gmail.com>
2021-09-26 18:38:26 -0400

    express routing and file rearrangement

--- server/index.js
+++ server/index.js
@@ -1,25 +1,37 @@
 /// --------------------------------------------- \\\
 /// --------------- Using Express --------------- \\\
 /// --------------------------------------------- \\\
-
 const express = require('express')
 const app = express()
+const { Router } = require('express')
+const path = require('path')
+const { auth, requiresAuth } = require('express-openid-connect')
 const port = 3000
-const { auth } = require('express-openid-connect')
-require('dotenv').config()
 
-app
-    .use(
-        auth({
-            issuerBaseURL: process.env.ISSUER_BASE_URL,
-            baseURL: process.env.BASE_URL,
-            clientID: process.env.CLIENT_ID,
-            secret: process.env.SECRET,
-            idpLogout: true,
-        }))
-    .get('/', (req, res) => {
-        res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-    })
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'UeCCWisLGBWq4wKneHuDYw1trHNDurfL',
+  issuerBaseURL: 'https://dev-fy741j28.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  //res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  res.sendFile(path.join(__dirname+'/docs/index.html'));
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+app.get('/logout', requiresAuth(), (req, res) => {
+    res.send('Bye');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
