dfd7295bdf5bb57ea2d8cf32aaeb7e111e904462
Chappellm <Chappelm1@hawkmail.newpaltz.edu>
2021-10-21 15:11:37 -0400

    rebuilt and working webserver

--- server/index.js
+++ server/index.js
@@ -7,18 +7,19 @@ const app = express()
 const port = 3000
 const path = require('path');
 const usersController = require('./controllers/users');
-/*const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'UeCCWisLGBWq4wKneHuDYw1trHNDurfL',
-  issuerBaseURL: 'https://dev-fy741j28.us.auth0.com'
-};
-*/
+
 const dirName = path.join(__dirname, '..', '/docs');
 app.use(express.static(dirName));
 
+
+app.use('/users', usersController);
+
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 
 
@@ -31,15 +32,15 @@ app.use(express.static(dirName));
 //app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   //res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
   res.sendFile(dirName + '/index.html');
-});
+});*/
 
-app.get('*', (req, res, next) => {
+/*pp.get('*', (req, res, next) => {
   console.log('a request came in');
   next();
-});
+});*/
 
 /*
 app.get('/profile', requiresAuth(), (req, res) => {
@@ -47,11 +48,14 @@ app.get('/profile', requiresAuth(), (req, res) => {
 });
 app.get('/logout', requiresAuth(), (req, res) => {
   res.send('Bye');
-});
+});*/
+
+/*const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'UeCCWisLGBWq4wKneHuDYw1trHNDurfL',
+  issuerBaseURL: 'https://dev-fy741j28.us.auth0.com'
+};
 */
-app.use('/users', usersController);
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
--- workoutapp/src/main.js
+++ workoutapp/src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bulma'
 
-import { Notification, Config } from '@oruga-ui/oruga-next';
+import { Notification } from '@oruga-ui/oruga-next';
 import '@oruga-ui/oruga-next/dist/oruga-full.css'
 
 
@@ -13,7 +13,5 @@ import router from './router'
 createApp(App)
     .use(router)
     .use(Notification)
-    .use(Config({
-        iconPack: 'fas'
-    }))
+
     .mount('#app')
